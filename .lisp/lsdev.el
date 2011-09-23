90d9a9986beadc178edbc7d7fc2377182e1a2c52
Sam Magnuson <smagnuso@gmail.com>
2011-09-23 14:16:55 -0700

    seems to work

--- .lisp/lsdev.el
+++ .lisp/lsdev.el
@@ -1,31 +1,10 @@
-;; (defun lsdev-parse-key-value(file)
-;;   (setq ret nil)
-;;   (if file
-;;       (with-temp-buffer
-;;         (insert-file-contents file)
-;;         (goto-char (point-min))
-;;         (while (re-search-forward "^\\(.*\\)=\\(.*\\)$" nil t)
-;;           (let ((key (buffer-substring (match-beginning 1) (match-end 1)))
-;;                 (value (buffer-substring (match-beginning 2) (match-end 2))))
-;;             (push (cons key value) ret)))))
-;;     ret)
-
-;; (defun lsdev-src-dirs()
-;;   (lsdev-parse-key-value "~/.dev_directories")
-;;   )
 
 (defun lsdev-dirs-internal (&rest match)
-  (interactive)
   (let ((result)
         (args '("-ta")))
-    (if match
-        (apply #'add-to-list 'args match))
-    ;; (set-buffer (create-file-buffer "/tmp/foo"))
-    ;; (message default-directory)
-    (message args)
+    (if match (dolist (m match) (add-to-list 'args m)))
     (with-temp-buffer
       (apply #'call-process (executable-find "lsdev.pl") nil t nil args)
-      (message (buffer-string))
       (goto-char (point-min))
       (while (not (eobp))
         (if (looking-at "^\\([^ ]*\\) \\[\\(.*\\)\\]$")
@@ -33,17 +12,17 @@
         (forward-line)))
     result))
 
-(defun lsdev-dirs-current-name (dir &rest match)
+(defun lsdev-name (dir &rest match)
   (save-excursion
     (if dir
         (cd dir))
-    (car (car (apply #'lsdev-dirs-internal "-p" match)))))
+    (nth 0 (car (apply #'lsdev-dirs-internal "-p" match)))))
 
-(defun lsdev-dirs-current-root-dir (dir &rest match)
+(defun lsdev-root-dir (dir &rest match)
   (save-excursion
     (if dir
         (cd dir))
-    (cdr (car (apply #'lsdev-dirs-internal "-r" "-p" match)))))
+    (nth 1 (car (apply #'lsdev-dirs-internal "-r" "-p" match)))))
 
 (defun lsdev-dirs-all (&rest match)
   (apply #'lsdev-dirs-internal "-a" "-l" match))
