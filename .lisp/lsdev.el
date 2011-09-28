f7cebbe20a971ed7c0f2231acb3261488492bea7
Sam Magnuson <smagnuso@gmail.com>
2011-09-27 21:23:25 -0700

    buffer-or-dir

--- .lisp/lsdev.el
+++ .lisp/lsdev.el
@@ -12,17 +12,19 @@
         (forward-line)))
     result))
 
-(defun lsdev-name (dir &rest match)
-  (save-excursion
-    (if dir
-        (cd dir))
-    (nth 0 (car (apply #'lsdev-dirs-internal "-p" match)))))
+(defun lsdev-name (buffer-or-dir &rest match)
+  (let ((dir))
+    (if (not (bufferp buffer-or-dir)) (setq dir buffer-or-dir) (if (buffer-file-name buffer-or-dir) (setq dir (file-name-directory (buffer-file-name buffer-or-dir)))))
+    (save-excursion
+      (if dir (progn (cd dir)
+                     (nth 0 (car (apply #'lsdev-dirs-internal "-p" match)))) nil))))
 
-(defun lsdev-root-dir (dir &rest match)
-  (save-excursion
-    (if dir
-        (cd dir))
-    (nth 1 (car (apply #'lsdev-dirs-internal "-r" "-p" match)))))
+(defun lsdev-root-dir (buffer-or-dir &rest match)
+  (let ((dir))
+    (if (not (bufferp buffer-or-dir)) (setq dir buffer-or-dir) (if (buffer-file-name buffer-or-dir) (setq dir (file-name-directory (buffer-file-name buffer-or-dir)))))
+    (save-excursion
+      (if dir (progn (cd dir)
+                     (nth 1 (car (apply #'lsdev-dirs-internal "-r" "-p" match)))) nil))))
 
 (defun lsdev-dirs-all (&rest match)
   (apply #'lsdev-dirs-internal "-a" "-l" match))
