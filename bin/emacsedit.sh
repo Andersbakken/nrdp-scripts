5c64a6ea8b8e08824a3d099db0868027a1732a09
Sam Magnuson <smagnuso@gmail.com>
2011-11-10 10:56:07 -0800

    my handy editing scripts

---
+++ bin/emacsedit.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+EMACS=
+EMACSWAIT=yes
+EMACSOPTS=""
+CONFIRM=no
+MODE=edit
+FILE=
+LINE=0
+COL=
+TEST=
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    -f) EMACS="emacs" ;;
+    -c) CONFIRM=yes ;;
+    -e) MODE=eval ;;
+    -t) MODE=tail ;;
+    -m) MODE=make ;;
+    -d) TEST=echo ;;
+    -n) EMACSWAIT=no ;;
+    -q) TEST=exists ;;
+    -nw) unset DISPLAY ;;
+    -h) echo "$0: [options] [file]"
+        echo "Options:"
+        echo "  -c    Must confirm execution"
+        echo "  -t    Tail provided file"
+        echo "  -e    Treat file as elisp and evaluate it"
+        exit 0
+        ;;
+    +*) LINE=`echo $1 | sed 's,^+,,'` ;;
+    -*) EMACSOPTS="$EMACSOPTS $1" ;;
+    *)  FILE="$1" ;;
+    esac
+    shift
+done
+[ -z "$FILE" ] && [ "$MODE" = "make" ] && FILE="$PWD"
+
+if [ -z "$EMACS" ]; then
+    if [ -n "$EMACSCLIENT" ]; then
+        EMACS="$EMACSCLIENT"
+    elif which gnuclient >/dev/null 2>&1 && gnuclient -v >/dev/null 2>&1; then
+        EMACS="gnuclient -q"
+    elif which emacsclient >/dev/null 2>&1; then
+        EMACS="emacsclient -a $(which emacs) -nw"
+        [ "$EMACSWAIT" = "no" ] && EMACS="$EMACS -n"
+    else
+        echo "No emacs client available!"
+        return
+    fi
+else
+    EMACS="$EMACS $EMACSOPTS"
+fi
+
+if [ -n "$FILE" ]; then
+    FILE=`$HOME/bin/findfile.sh $FILE`
+    if [ "CONFIRM" = "yes" ]; then
+        echo -n "$FILE"
+        read confirm
+        [ -n "$confirm" ] && return
+    fi
+    if echo "$FILE" | grep ':' >/dev/null 2>&1; then
+        COL=`echo $FILE | cut -d: -f3`
+        LINE=`echo $FILE | cut -d: -f2`
+        FILE=`echo $FILE | cut -d: -f1`
+    fi
+    if [ "$TEST" = "exists" ]; then
+        [ -e "$FILE" ] && exit 0
+        exit 1
+    elif [ "$MODE" = "eval" ]; then
+        $TEST $EMACS -e "$FILE"
+    elif [ "$MODE" = "make" ]; then
+        $TEST $EMACS -e "(sam-compile-directory \"$FILE\")"
+    elif [ "$MODE" = "tail" ]; then
+        $TEST $EMACS -e "(tailf \"$FILE\")"
+    else
+        JUMP=
+        if [ -n "$LINE" ]; then
+            [ -z "$COL" ] && COL="0"
+            JUMP="${LINE}:${COL}"
+        else
+            JUMP=0
+        fi
+        $TEST $EMACS "+${JUMP}" "$FILE"
+    fi
+elif [ "$TEST" != "exists" ]; then
+    $TEST $EMACS
+fi
+exit 1
---
+++ bin/findfile.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+COL=
+LINE=
+FILE="$1"
+if echo "$FILE" | grep ':$' >/dev/null 2>&1; then
+    FILE=`echo "$FILE" | sed "s,:$,,"`
+fi
+if echo "$FILE" | grep ':' >/dev/null 2>&1; then
+    COL=`echo $FILE | cut -d: -f3`
+    LINE=`echo $FILE | cut -d: -f2`
+    FILE=`echo $FILE | cut -d: -f1`
+fi
+
+if echo "$FILE" | grep "^//" >/dev/null 2>&1; then
+    P4FILE=`echo $FILE | sed "s,[@#].*$,,g"`
+    W=`p4 where "$P4FILE" 2>&1`
+    if echo "$W" | grep 'not in client view' >/dev/null 2>&1; then
+        TEMP=`mktemp`
+        p4 print "$P4FILE" >"$TEMP" && FILE="$TEMP"
+    else
+        FILE=`echo $W | awk '{print $3}'`
+    fi
+elif [ ! -e "$FILE" ] && FILES=`global -P "$FILE" 2>/dev/null`; then
+    [ -n "$FILES" ] && FILE="$($HOME/bin/choose.pl $FILES)"      
+fi
+if [ -z "$LINE" ]; then
+    echo "${FILE}"
+elif [ -z "$COL" ]; then
+    echo "${FILE}:${LINE}"
+else
+    echo "${FILE}:${LINE}:${COL}"
+fi
