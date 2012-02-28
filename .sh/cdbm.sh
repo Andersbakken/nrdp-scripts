8202baef52ca99810896151dc4f717d1e051892b
Sam Magnuson <smagnuso@gmail.com>
2012-02-27 20:29:15 -0800

    cdl

--- .sh/cdbm.sh
+++ .sh/cdbm.sh
@@ -7,6 +7,16 @@ cd() {
        builtin cd "$@"
    fi
 }
+cdl() {
+    dir="$1"
+    [ -z "$dir" ] && dir="$PWD"
+    for d in `ls -lt $dir`; do
+        if [ -d "$d" ]; then
+            cd "$d"
+            break
+        fi
+    done
+}
 cdo() {
    eval DIR="$1"
    builtin cd "$DIR"
