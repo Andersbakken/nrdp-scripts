c3a13d573c61c35efcec5ad5b9b4715c0c9857d0
Anders Bakken <agbakken@gmail.com>
2013-04-25 10:37:26 -0700

    Sudo open

--- .lisp/misc.el
+++ .lisp/misc.el
@@ -501,3 +501,8 @@ the name of the value of file-name is present."
   nil)
 (add-hook 'write-file-hooks 'sam-c-clean-out-spaces-hooked)
 
+(defun sudo-find-file (file-name)
+  "Like find file, but opens the file as root."
+  (interactive "FSudo Find File: ")
+  (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
+    (find-file tramp-file-name)))
