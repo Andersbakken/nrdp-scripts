65cef840fed97bc28bc807231403db7a7c2ee183
Anders Bakken <agbakken@gmail.com>
2014-01-08 11:43:49 -0800

    Untar for me.

--- bin/nrdp.sh
+++ bin/nrdp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version=main
-output=netflix
+output=
 
 while [ -n "$1" ]; do
     case "$1" in
@@ -9,7 +9,7 @@ while [ -n "$1" ]; do
             echo "$0 Usage"
             echo "./nrdp.sh [args]..."
             echo "  --version=[arg]                   Override version (default $version)"
-            echo "  --output=[arg]                    Set output file (default $output)"
+            echo "  --output=[arg]                    Set output file (default nrdp-$version)"
             echo "  --build=[arg]                     Which Jenkins build to use (default last)"
             exit 0
             ;;
@@ -34,15 +34,21 @@ done
 [ -z $build ] && build=`curl "http://builds.netflix.com/view/PPD/view/PPD-NRDAPP/view/all/job/PPD-NRDAPP-$version/" 2> /dev/null \
                         | grep "Last successful build (#[0-9]" \
                         | sed -e 's,^.*Last successful build (#\([0-9]\+\).*$,\1,'`
-if [ -f "$output" ]; then
+test -z "$output" && output="nrdp-$version"
+if [ -d "$output" ]; then
     echo -n "$output exists, are you sure you want to overwrite? (Y/N): "
     read answer
     case "$answer" in
         yes|y|Y)
+            rm -rf "$output"
             ;;
         *)
             exit 1
             ;;
     esac
 fi
-wget "http://builds.netflix.com/view/PPD/view/PPD-NRDAPP/view/all/job/PPD-NRDAPP-$version/lastSuccessfulBuild/artifact/nrdapp_$version-Debug.tar.gz" -O "$output"
+
+mkdir "$output"
+cd "$output"
+wget "http://builds.netflix.com/view/PPD/view/PPD-NRDAPP/view/all/job/PPD-NRDAPP-$version/lastSuccessfulBuild/artifact/nrdapp_$version-Debug.tar.gz" -O "nrdapp_$version-Debug.tar.gz"
+tar xf "nrdapp_$version-Debug.tar.gz"
