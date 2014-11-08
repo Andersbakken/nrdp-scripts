ea774107558fe0e91bc88e780f169bd956501849
Anders Bakken <agbakken@gmail.com>
2014-11-08 09:24:55 -0800

    Make sure lsdev.pl is in the path before calling it

--- .lisp/lsdev.el
+++ .lisp/lsdev.el
@@ -215,10 +215,12 @@
 (defun lsdev-cd(&optional ignore-builds)
   (interactive)
   (let* ((args nil)
+         (exec (executable-find "lsdev.pl"))
          (from-eshell (and (eq major-mode 'eshell-mode) (current-buffer)))
-         (alternatives (with-temp-buffer
-                         (call-process (executable-find "lsdev.pl") nil (list t nil) nil "-a" "-l" "-tn" (if lsdev-cd-ignore-builds "-build" ""))
-                         (cl-remove-duplicates (split-string (buffer-string) "[\f\t\n\r\v_-]+") :test 'equal)))
+         (alternatives (and exec
+                            (with-temp-buffer
+                              (call-process (executable-find "lsdev.pl") nil (list t nil) nil "-a" "-l" "-tn" (if lsdev-cd-ignore-builds "-build" ""))
+                              (cl-remove-duplicates (split-string (buffer-string) "[\f\t\n\r\v_-]+") :test 'equal))))
          (hd (ido-completing-read "LSDEV Directory: " alternatives nil t nil 'lsdev-cd-history)))
     (setq lsdev-cd-history (cl-remove-duplicates lsdev-cd-history :from-end t :test 'equal))
     (push "-a" args)
