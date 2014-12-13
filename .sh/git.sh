5c59023e63bc84e96ff6e482b03a40d54fae89a2
Sam Magnuson <smagnuso@gmail.com>
2014-12-12 16:34:08 -0800

    add push

--- .sh/git.sh
+++ .sh/git.sh
@@ -1,32 +1,25 @@
 #sync a tree
 gs()
 {
+    ACTION="sync -r"
     CDD_FLAGS="src "
     while [ "$#" -gt 0 ]; do
       case "$1" in
+      --status) ACTION="status" ;;
+      --push) shift; ACTION="push $1" ;;
+      --pushf) shift; ACTION="push -f $1" ;;
       *) CDD_FLAGS="$CDD_FLAGS $1" ;;
       esac
       shift
     done
-
     if [ "`lsdev.pl -r -l $CDD_FLAGS | wc -l`" != "1" ] && $(git rev-parse --git-dir &> /dev/null); then
-        git sync -r --autostash
+        git $ACTION
     else
-        git cdd $CDD_FLAGS -- sync -r
+        git cdd $CDD_FLAGS -- $ACTION
     fi
-}
-
-gp4_sync()
-{
-   P4CHANGE=`git p4 change HEAD | awk '{print $4}'`
-   echo "Syncing to: $P4CHANGE"
-   for m in $@; do
-       (cd $m; GITCHANGE=`git p4 change $P4CHANGE | awk '{print $4}'`; echo "Sync $m @ $GITCHANGE ($P4CHANGE)"; git checkout "$GITCHANGE")
-   done
 }
 
 gss()
 {
-    CDD_FLAGS="-r"
-    git cdd $CDD_FLAGS -- status $PUSH_FLAGS
+    gs --status "$@"
 }
