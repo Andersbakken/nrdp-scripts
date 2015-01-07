a95bee9cd2a7d3ed781844be9472b452d24d24c4
Sam Magnuson <smagnuso@gmail.com>
2015-01-07 15:55:37 -0800

    don't require alias

--- .sh/git.sh
+++ .sh/git.sh
@@ -2,20 +2,20 @@
 gs()
 {
     ACTION="sync -r"
-    CDD_FLAGS="src "
+    LSDEV_FLAGS="src "
     while [ "$#" -gt 0 ]; do
       case "$1" in
       --status) ACTION="status" ;;
       --push) shift; ACTION="push $1" ;;
       --pushf) shift; ACTION="push -f $1" ;;
-      *) CDD_FLAGS="$CDD_FLAGS $1" ;;
+      *) LSDEV_FLAGS="$LSDEV_FLAGS $1" ;;
       esac
       shift
     done
-    if [ "`lsdev.pl -r -l $CDD_FLAGS | wc -l`" != "1" ] && $(git rev-parse --git-dir &> /dev/null); then
+    if [ "`lsdev.pl -r -l $LSDEV_FLAGS | wc -l`" != "1" ] && $(git rev-parse --git-dir &> /dev/null); then
         git $ACTION
     else
-        git cdd $CDD_FLAGS -- $ACTION
+        git lsdev $LSDEV_FLAGS -- $ACTION
     fi
 }
 
