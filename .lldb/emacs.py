2a87f00f1d1fab08586d66d8ff8a0c47a09071b3
Sam Magnuson <smagnuso@gmail.com>
2015-06-04 23:24:37 -0700

    emacs too

---
+++ .lldb/emacs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import lldb
+import os
+
+def emake(debugger, command, result, internal_dict):
+  os.system("emacsedit.sh -m -n")
+
+def ecd(debugger, command, result, internal_dict):
+  os.system("emacsedit.sh -n $PWD")
+
+def ebreak(debugger, command, result, internal_dict):
+  pipe = os.popen("emacsedit.sh -n -e '(sam-what-file)'", 'rb')
+  result = pipe.read().rstrip()
+  pipe.close()
+  debugger.HandleCommand("br %s" % result)
+
+def __lldb_init_module (debugger, dict):
+  debugger.HandleCommand('command script add -f emacs.ebreak ebreak')
+  debugger.HandleCommand('command script add -f emacs.emake emake')
+  debugger.HandleCommand('command script add -f emacs.ecd ecd')
