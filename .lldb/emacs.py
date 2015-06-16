c659a10de8ac2f92e4293fbfb1f43ca834929878
Sam Magnuson <smagnuso@gmail.com>
2015-06-15 18:10:21 -0700

    stub

--- .lldb/emacs.py
+++ .lldb/emacs.py
@@ -6,6 +6,9 @@ import os
 def emake(debugger, command, result, internal_dict):
   os.system("emacsedit.sh -m -n")
 
+def rez(debugger, command, result, internal_dict):
+  print "%s" % debugger.frame.file.fullpath;
+
 def ecd(debugger, command, result, internal_dict):
   os.system("emacsedit.sh -n $PWD")
 
@@ -16,6 +19,7 @@ def ebreak(debugger, command, result, internal_dict):
   debugger.HandleCommand("br %s" % result)
 
 def __lldb_init_module (debugger, dict):
+  debugger.HandleCommand('command script add -f emacs.rez rez')
   debugger.HandleCommand('command script add -f emacs.ebreak ebreak')
   debugger.HandleCommand('command script add -f emacs.emake emake')
   debugger.HandleCommand('command script add -f emacs.ecd ecd')
