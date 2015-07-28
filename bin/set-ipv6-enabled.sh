26e0fb0b2b92dad6a9c8d692d3440b4a11d95b51
Anders Bakken <agbakken@gmail.com>
2015-07-28 09:13:45 -0700

    Oops.

--- bin/set-ipv6-enabled.sh
+++ bin/set-ipv6-enabled.sh
@@ -3,10 +3,10 @@
 var=
 case "$1" in
     on|1|enabled|true)
-        var=1
+        var=0
         ;;
     off|0|disabled|false)
-        var=0
+        var=1
         ;;
     *)
         echo "Invalid option. Usage: $0 (on|1|enabled|true|off|0|disabled|false)"
