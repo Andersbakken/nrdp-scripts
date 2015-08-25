d872c55c36b6065dead5e37058e64b8e3f30e586
Anders Bakken <agbakken@gmail.com>
2015-08-25 13:48:40 -0700

    Oopsoops.

--- .lisp/nrdp-git.el
+++ .lisp/nrdp-git.el
@@ -491,7 +491,7 @@
   (interactive)
   (let ((prev (getenv "GIT_POST_SUBMIT_NON_INTERACTIVE")))
     (setenv "GIT_POST_SUBMIT_NON_INTERACTIVE" "1")
-    (magit-run-git-async "submit" commit)
+    (magit-run-on-multiple "submit" commit)
     (setenv "GIT_POST_SUBMIT_NON_INTERACTIVE" prev)))
 
 (defun magit-ignore (&optional commit)
