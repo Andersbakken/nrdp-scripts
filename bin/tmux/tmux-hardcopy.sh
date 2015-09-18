7648a679d0c5291a639657cd0a9b95451b9f6c54
Sam Magnuson <smagnuso@gmail.com>
2015-09-17 22:43:44 -0700

    not needed?

--- bin/tmux/tmux-hardcopy.sh
+++ bin/tmux/tmux-hardcopy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 rm -f "$1"
-tmux -q capture-pane -S -32768 \; save-buffer -b 0 $1 \; delete-buffer -b 0 \; display-message "Captured to: $1"
+tmux -q capture-pane -S -32768 \; save-buffer $1 \; display-message "Captured to: $1"
