d7e01b0d30feb1f5f6162c32c38e880752761ef8
Anders Bakken <agbakken@gmail.com>
2015-10-16 13:59:20 -0700

    Give me a chance to pop up a thing on my desktop when the build finishes

--- bin/ubermake.sh
+++ bin/ubermake.sh
@@ -16,6 +16,17 @@ findancestor() {
         done)
     return 0
 }
+SUCCESS_POST_COMMAND=
+ERROR_POST_COMMAND=
+
+finish() {
+    if [ "$1" -eq 0 ]; then
+        [ -n "$SUCCESS_POST_COMMAND" ] && eval "$SUCCESS_POST_COMMAND"
+    else
+        [ -n "$ERROR_POST_COMMAND" ] && eval "$ERROR_POST_COMMAND"
+    fi
+    exit "$1"
+}
 
 MAKE_DIR=
 MAKE_OPTIONS=
@@ -28,6 +39,8 @@ while [ "$#" -gt 0 ]; do
         -r|--rtags) UBERTAGS=1 ;;
         -v) MAKE_DIR="${PWD}/" ;; #disable lsdev
         -l) shift; LSDEV_ARGS="$LSDEV_ARGS $1" ;;
+        -s) shift; SUCCESS_POST_COMMAND="$1" ;;
+        -e) shift; ERROR_POST_COMMAND="$1" ;;
         *) MAKE_OPTIONS="$MAKE_OPTIONS $1" ;;
     esac
     shift
@@ -80,7 +93,7 @@ else
         # echo "FOUND IT $COMPILATION_DATABASEJSON"
         if [ -e "$COMPILATION_DATABASEJSON" ]; then
             rc -J "$COMPILATION_DATABASEJSON"
-            exit 0
+            finish 0
         fi
     fi
     if which ninja >/dev/null 2>&1; then
@@ -91,7 +104,7 @@ else
             cd `dirname $NINJA`
             if [ -n "$UBERTAGS" ]; then
                 ninja -t commands | rc --compile
-                exit 0
+                finish 0
             fi
             NINJA_OPTIONS=
             [ "$VERBOSE" = "1" ] && NINJA_OPTIONS="$NINJA_OPTIONS -v"
@@ -103,7 +116,7 @@ else
             done
             NINJA_OPTIONS=`echo $NINJA_OPTIONS | sed -e 's,-j ,-j1000 ,g' -e 's,-j$,-j1000,'`
             ninja $NINJA_OPTIONS
-            exit $?
+            finish $?
         fi
     fi
 fi
@@ -111,7 +124,7 @@ fi
 [ -z "$MAKE_DIR" ] && MAKE_DIR=.
 if [ -n "$UBERTAGS" ]; then
     RTAGS_RMAKE=1 `which make` -C "$MAKE_DIR" -B
-    exit 0
+    finish 0
 fi
 `which make` -C "$MAKE_DIR" $MAKE_OPTIONS #go for the real make
-exit $?
+finish $?
