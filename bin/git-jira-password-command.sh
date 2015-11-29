bdcd62d44660451f84d22fc2c2d02aad8d99a181
Jan Erik Hanssen <jhanssen@gmail.com>
2015-11-29 15:35:51 -0800

    Use gpg2 if available

--- bin/git-jira-password-command.sh
+++ bin/git-jira-password-command.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+[[ $(type -P "gpg2") ]] && GPG="gpg2" || GPG="gpg"
+
 while [ -n "`ps aux | grep "\<gpg[^-]" | grep -v grep`" ]; do # apparently pidof gpg returns pid for gpg-agent
     sleep 1
 done
 
-password=`gpg --no-tty -d "$1" | tail -n 1`
+password=`${GPG} --no-tty -d "$1" | tail -n 1`
 echo -n "$password"
