b5f0975e2ec33aa052db6967f22bc58753d02956
Sam Magnuson <smagnuso@gmail.com>
2016-08-03 13:10:13 -0700

    shared now

---
+++ .sh/history.sh
@@ -0,0 +1,37 @@
+shopt -s histappend
+
+export HISTSIZE=500000
+export HISTFILESIZE=5000000
+export HISTCONTROL=ignoredups
+export HISTIGNORE=$'&'
+
+history_share_auto()
+{
+    if [ "$#" = 0 ]; then
+        export HISTORY_SHARE_AUTO_READ=1
+        export HISTORY_SHARE_AUTO_WRITE=1
+    else
+        for a in $@; do
+            if [ "$a" = "read" ]; then
+                export HISTORY_SHARE_AUTO_READ=1
+            elif [ "$a" = "noread" ]; then
+                export HISTORY_SHARE_AUTO_READ=0
+            elif [ "$a" = "write" ]; then
+                export HISTORY_SHARE_AUTO_WRITE=1
+            elif [ "$a" = "nowrite" ]; then
+                export HISTORY_SHARE_AUTO_WRITE=0
+            fi
+        done
+    fi
+}
+
+cmd_history_share_auto()
+{
+    [ "$HISTORY_SHARE_AUTO_WRITE" = "1" ] && history -a
+    [ "$HISTORY_SHARE_AUTO_READ" = "1" ] && history -n
+}
+
+history_share_auto read write
+add_prompt_command "cmd_history_share_auto"
+
+
