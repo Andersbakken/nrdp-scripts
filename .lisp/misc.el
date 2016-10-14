831b55f15145e355cb57abd4642c87311f76467b
Anders Bakken <agbakken@gmail.com>
2016-10-13 21:26:57 -0700

    Use d for copy both in ediff

--- .lisp/misc.el
+++ .lisp/misc.el
@@ -172,6 +172,15 @@
   (interactive)
   (--misc-replace-string-helper "\n" "\r\n" ))
 
+(defun ediff-copy-both-to-C ()
+  (interactive)
+  (ediff-copy-diff ediff-current-difference nil 'C nil
+                   (concat
+                    (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                    (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
+
 (defun ediff-cleanup-buffer-b-handler ()
   (if ediff-buffer-B
       (let ((b-window (get-buffer-window ediff-buffer-B)))
