8c08059d1ba6631ea7a82dbf3d2743ba94a6522a
Anders Bakken <agbakken@gmail.com>
2016-11-14 10:17:10 -0800

    Allow passing args to java in jira.sh

--- bin/jira-cli-2.6.0/jira.sh
+++ bin/jira-cli-2.6.0/jira.sh
@@ -4,4 +4,4 @@
 # - Customize for your installation, for instance you might want to add default parameters like the following:
 # java -jar `dirname $0`/lib/jira-cli-2.6.0.jar --server http://my-server --user automation --password automation "$@"
 
-java -jar `dirname $0`/lib/jira-cli-2.6.0.jar "$@"
+java $JIRACLIARGUMENTS -jar `dirname $0`/lib/jira-cli-2.6.0.jar "$@"
