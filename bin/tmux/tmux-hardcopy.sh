1e60fbf0bb9e1a42aaf5789f8f25cb43817e3227
Sam Magnuson <smagnuso@gmail.com>
2017-09-21 10:30:24 -0700

    moved the emacs edit into the tmux hardcopy script to make it easier to debug

--- bin/tmux/tmux-hardcopy.sh
+++ bin/tmux/tmux-hardcopy.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+EDIT="no"
+if [ "$1" = "-e" ]; then
+    EDIT="yes"
+    shift
+fi
+
 rm -f "$1"
 tmux -q capture-pane -S -32768 \; save-buffer $1 \; display-message "Captured to: $1"
+
+if [ "$EDIT" = "yes" ]; then
+    emacsedit.sh -n "$1" >/dev/null 2>&1
+fi
