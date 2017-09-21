40fce26e9ea07b3a01bef1ce88d1d0201e6f00c4
Sam Magnuson <smagnuso@gmail.com>
2017-09-21 10:46:39 -0700

    do not wrap

--- bin/tmux/tmux-hardcopy.sh
+++ bin/tmux/tmux-hardcopy.sh
@@ -6,7 +6,7 @@ if [ "$1" = "-e" ]; then
 fi
 
 rm -f "$1"
-tmux -q capture-pane -S -32768 \; save-buffer $1 \; display-message "Captured to: $1"
+tmux -q capture-pane -J -S -32768 \; save-buffer $1 \; display-message "Captured to: $1"
 
 if [ "$EDIT" = "yes" ]; then
     emacsedit.sh -n "$1" >/dev/null 2>&1
