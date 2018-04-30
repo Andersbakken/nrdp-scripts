12ff6152fd85b956144e7f58c5bd8adca298c641
Anders Bakken <agbakken@gmail.com>
2018-04-30 09:51:21 -0700

    Add pvm.el

---
+++ .lisp/pvm.el
@@ -0,0 +1,21 @@
+(defun pvm-cd (&optional prefix func)
+  (interactive "p")
+  (let ((opts nil))
+    (with-temp-buffer
+      (call-process "pvm" nil t nil "ls")
+      ;; (message "GOT SHIT:\n%s" (buffer-string))
+      (goto-char (point-min))
+      (while (re-search-forward "\\([^ ]+@[0-9]+\\) *-" nil t)
+        ;; (message "Got line %s" (buffer-substring-no-properties (point-at-bol) (match-end 1)))
+        ;; (message "GOT THING [%s]" (match-string 1))
+        (push (match-string 1) opts)
+        (forward-char)))
+    (if (null opts)
+        (error "No pvms found")
+      (let ((result (completing-read "PVM: " opts nil t)))
+        (when result
+          (let ((dir (shell-command-to-string (concat "pvm show --installpath " result))))
+            (when dir
+              (funcall (or func 'find-file) (if (string-match "\n$" dir) (substring dir 0 -1) dir)))))))))
+
+(provide 'pvm)
