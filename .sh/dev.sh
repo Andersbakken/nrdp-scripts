35c0b9514072e06ed5510056abb46ceac681d23d
Sam Magnuson <smagnuso@gmail.com>
2018-05-07 09:25:37 -0700

    dead

--- .sh/dev.sh
+++ .sh/dev.sh
@@ -124,7 +124,6 @@ function findcmake()
 
 block-icecream() {
     [ -e "/etc/icecc/icecc.conf" ] && . /etc/icecc/icecc.conf
-    [ -z "$ICECC_SCHEDULER_HOST" ] && ICECC_SCHEDULER_HOST="lgux-pnavarro3.corp.netflix.com"
     echo blockcs "$1" | nc "$ICECC_SCHEDULER_HOST" 8766
 }
 
