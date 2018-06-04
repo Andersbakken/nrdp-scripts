2d0078f787d625093cc43038af9887ab3c6876ea
Anders Bakken <agbakken@gmail.com>
2018-06-04 11:23:51 -0700

    gooder

--- bin/preprocess.sh
+++ bin/preprocess.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ ! -e "$1" ] || [ ! -x "$1" ]; then ### single source file
+    while [ ! -e build.ninja ] && [ `pwd` != "/" ]; do
+        cd ..
+    done
+    if [ ! -e build.ninja ]; then
+        echo "Can't find build.ninja"
+        exit 1
+    fi
+    "$0" `ninja -t commands | grep "$1" | head -n1`
+    exit $?
+fi
+
 ARGS=()
 while [ -n "$1" ]; do
     if [ "$1" = "-o" ]; then
