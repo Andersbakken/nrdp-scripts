cfabdcda691ec5af46099416507fefabaf8aef7a
Anders Bakken <agbakken@gmail.com>
2018-08-01 10:38:21 -0700

    More wip

--- nrdp-jira/cli.js
+++ nrdp-jira/cli.js
@@ -1,19 +1,29 @@
 #!/usr/bin/env node
 
 const http = require('http');
+const child_process = require('child_process');
 
-var req = http.get("http://localhost:58910", resp => {
-    console.log(resp);
-    console.log(error);
-});
+let spawned = false;
+function connect()
+{
+    var req = http.get("http://localhost:58910", resp => {
+        console.log("we're connected resp");
+    });
 
-req.on('error', error => {
-    // console.log("Got error", error);
-    if (error.code == 'ECONNREFUSED') {
-
-    } else {
-        console.log("Unknown error", error);
-    }
-});
+    req.on('error', error => {
+        // console.log("Got error", error);
+        if (error.code == 'ECONNREFUSED') {
+            if (spawned) {
+                console.log("ECONNREFUSED starting daemon");
+                const daemon = child_process.spawn('node', `$(__dirname)/server.js`);
+            } else {
+                console.log("Waiting for daemon");
+                setTimeout(connect, 1000);
+            }
+        } else {
+            console.log("Unknown error", error);
+        }
+    });
+}
 
 
