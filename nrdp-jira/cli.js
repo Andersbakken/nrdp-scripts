35075ae8cf85a741c08403cee524045472c823db
Jan Erik Hanssen <jhanssen@gmail.com>
2018-08-01 14:51:22 -0700

    stuff

--- nrdp-jira/cli.js
+++ nrdp-jira/cli.js
@@ -1,29 +1,130 @@
 #!/usr/bin/env node
 
-const http = require('http');
-const child_process = require('child_process');
+const WebSocket = require("ws");
+const child_process = require("child_process");
+const argv = require("minimist")(process.argv.slice(2));
+const gpg = require("gpg");
+const readline = require("readline");
+const Writable = require("stream").Writable;
+
+let muted = false;
+const mutedStdout = new Writable({
+    write: function(chunk, encoding, callback) {
+        if (!muted)
+            process.stdout.write(chunk, encoding);
+        callback();
+    }
+});
+
+function readPassword()
+{
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: mutedStdout,
+            terminal: true
+        });
+
+        muted = false;
+        rl.question("Password: ", pwd => {
+            // terminate the muted output with a newline
+            console.log("");
+
+            rl.close();
+            resolve(pwd);
+        });
+        muted = true;
+    });
+}
+
+const port = argv.port || 58910;
 
 let spawned = false;
 function connect()
 {
-    var req = http.get("http://localhost:58910", resp => {
-        console.log("we're connected resp");
+    let action;
+    if (argv.comment) {
+        action = { comment: argv.comment };
+    }
+    if (argv.resolve) {
+        if (!action)
+            action = {};
+        action.resolve = argv.resolve;
+    }
+
+    if (!action) {
+        console.error("need comment or resolve");
+        return;
+    }
+    if (!argv.issue) {
+        console.error("need an issue");
+        return;
+    }
+
+    action.issue = argv.issue;
+
+    const ws = new WebSocket(`ws://localhost:${port}`);
+
+    ws.on("open", () => {
+        try {
+            ws.send(JSON.stringify(action));
+        } catch (err) {
+            console.error("failed to send action to server", err);
+            process.exit();
+        }
     });
 
-    req.on('error', error => {
-        // console.log("Got error", error);
-        if (error.code == 'ECONNREFUSED') {
-            if (spawned) {
-                console.log("ECONNREFUSED starting daemon");
-                const daemon = child_process.spawn('node', `$(__dirname)/server.js`);
+    ws.on("message", json => {
+        let msg;
+        try {
+            msg = JSON.parse(json);
+        } catch (err) {
+        }
+
+        if (msg.needsPassword) {
+            console.log("gotta send stuff");
+            if (process.env["JIRA_PASSWORD_FILE"]) {
+                console.log("hehhh");
+                gpg.decryptFile(process.env["JIRA_PASSWORD_FILE"], (err, contents) => {
+                    console.log("hehhh2");
+                    ws.send(JSON.stringify({
+                        username: process.env.USER,
+                        password: contents.toString("utf8").trim()
+                    }));
+                });
+                console.log("hehhh3");
             } else {
-                console.log("Waiting for daemon");
-                setTimeout(connect, 1000);
+                readPassword().then(pwd => {
+                    console.log("hohhh2");
+                    ws.send(JSON.stringify({
+                        username: process.env.USER,
+                        password: pwd
+                    }));
+                });
             }
+        }
+        if ("success" in msg) {
+            console.log("got final response", msg);
+            process.exit(msg.success ? 0 : 1);
+        }
+    });
+
+    ws.on("error", error => {
+        console.log("Got error", error);
+        if (error.code == 'ECONNREFUSED') {
+            if (!spawned) {
+                console.log("ECONNREFUSED starting daemon");
+                const daemon = child_process.spawn("node", [`${__dirname}/server.js`, "--port", port], { detached: true });
+                daemon.unref();
+
+                spawned = true;
+            }
+            console.log("Waiting for daemon");
+            setTimeout(connect, 100);
         } else {
             console.log("Unknown error", error);
         }
     });
 }
 
-
+connect();
---
+++ nrdp-jira/server.js
@@ -0,0 +1,118 @@
+const fs = require("fs");
+const WebSocket = require("ws");
+const argv = require("minimist")(process.argv.slice(2));
+const Jira = require("jira-client");
+
+const port = argv.port || 58910;
+
+const opts = {};
+
+function log(...args) {
+    for (let a of args) {
+        let m;
+        try {
+            m = JSON.stringify(a, null, 4);
+        } catch (e) {
+            m = Object.keys(a);
+        }
+        fs.appendFileSync("/tmp/git-jira-server.log", m + " ");
+    }
+    fs.appendFileSync("/tmp/git-jira-server.log", "\n");
+}
+
+console.log = log;
+
+const wss = new WebSocket.Server({ port: port });
+wss.on("connection", ws => {
+    let pending = [];
+
+    const processPending = () => {
+        for (let i = 0; i < pending.length; ++i) {
+            let msg = pending[i];
+
+            const ok = (msg, data) => {
+                try {
+                    ws.send(JSON.stringify({ success: true,
+                                             msg: msg,
+                                             data: data }));
+                } catch (err) {
+                    log("Failed to ok message to client", JSON.stringify(msg));
+                }
+            };
+            const error = (msg, err) => {
+                try {
+                    ws.send(JSON.stringify({ success: false,
+                                             msg: msg,
+                                             error: err }));
+                } catch (err) {
+                    log("Failed to error message to client", JSON.stringify(msg));
+                }
+            };
+
+            if (msg.issue) {
+                if (msg.resolve) {
+                    let cmd = { transition: { id: 5 } };
+                    if (msg.comment)
+                        cmd.update = { comment: [ { add: { body: msg.comment } } ] };
+                    opts.jira.transitionIssue(msg.issue, cmd).then(transition => {
+                        ok(msg, transition);
+                    }).catch(err => {
+                        error(msg, err);
+                    });
+                } else if (msg.comment) {
+                    console.log("about to comment", msg.issue, msg.comment);
+                    opts.jira.addComment(msg.issue, msg.comment).then(comment => {
+                        ok(msg, comment);
+                    }).catch(err => {
+                        error(msg, err);
+                    });
+                }
+                /*
+                opts.jira.findIssue(msg.issue).then(issue => {
+                    ok(msg, issue);
+                }).catch(err => {
+                    error(msg, err);
+                });
+                */
+            }
+        }
+        pending = [];
+    };
+
+    ws.on("message", json => {
+        let msg;
+        try {
+            msg = JSON.parse(json);
+        } catch (err) {
+        }
+
+        if (msg.username) {
+            opts.username = msg.username;
+            opts.password = msg.password;
+
+            const jiraopts = {
+                protocol: "https",
+                host: "jira.netflix.com",
+                username: opts.username,
+                password: opts.password,
+                apiVersion: "2",
+                strictSSL: true
+            };
+            //log("welp", jiraopts);
+
+            opts.jira = new Jira(jiraopts);
+            processPending();
+
+            //log("got foo", opts.username, opts.password);
+            return;
+        }
+
+        pending.push(msg);
+        if (opts.jira)
+            processPending();
+    });
+    if (!opts.jira) {
+        ws.send(JSON.stringify({ needsPassword: true }));
+        return;
+    }
+});
