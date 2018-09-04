753a46b688d7eb048f084222814572cbfb2a1efa
Sam Magnuson <smagnuso@gmail.com>
2018-09-04 11:11:23 -0700

    put pvm into lsdev

--- .sh/lsdev.sh
+++ .sh/lsdev.sh
@@ -1,128 +1,128 @@
 add_lsdev() {
-  DIR=
-  NAME=
-  FORCE=no
-  ACTION=add
-  FILE=
-  while [ "$#" -gt 0 ]; do
-     case "$1" in
-     -file) shift; FILE="$1" ;;
-     -f) FORCE=yes ;;
-     -d) ACTION=del ;;
-     *)
-        if [ -z "$DIR" ]; then
-            DIR="$1"
-            echo "$DIR" | grep -v -q "^/" && DIR="$PWD/$DIR"
-        elif [ -z "$NAME" ]; then
-            NAME="$1"
-        fi
-        ;;
-     esac
-     shift
-  done
-  if [ -z "$FILE" ]; then
-      echo "No file specified!"
-      return 1
-  fi
-  if echo "$DIR" | grep '^-' >/dev/null 2>&1; then
-      ACTION=del
-      DIR=`echo "$DIR" | sed 's,^-,,'`
-      NAME="$DIR"
-  fi
-  [ -n "$DIR" ] && [ -z "$NAME" ] && NAME=`basename $DIR`
+    local DIR=
+    local NAME=
+    local FORCE=no
+    local ACTION=add
+    local FILE=
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            -file) shift; FILE="$1" ;;
+            -f) FORCE=yes ;;
+            -d) ACTION=del ;;
+            *)
+                if [ -z "$DIR" ]; then
+                    DIR="$1"
+                    echo "$DIR" | grep -v -q "^/" && DIR="$PWD/$DIR"
+                elif [ -z "$NAME" ]; then
+                    NAME="$1"
+                fi
+                ;;
+        esac
+        shift
+    done
+    if [ -z "$FILE" ]; then
+        echo "No file specified!"
+        return 1
+    fi
+    if echo "$DIR" | grep '^-' >/dev/null 2>&1; then
+        ACTION=del
+        DIR=`echo "$DIR" | sed 's,^-,,'`
+        NAME="$DIR"
+    fi
+    [ -n "$DIR" ] && [ -z "$NAME" ] && NAME=`basename $DIR`
 
-  if [ "$ACTION" = "del" ]; then
-      if [ -z "$FILE" ]; then
-          echo "No shadows."
-      elif grep "^${DIR}=" "$FILE" >/dev/null 2>&1; then
-          echo "Removed. ${FILE}"
-          sed -e "s,^${DIR}=.*\$,," -e "/^$/d" -i "$FILE"
-      elif grep "^.*=${DIR}\$" "$FILE" >/dev/null 2>&1; then
-          echo "Removed. ${FILE}"
-          sed -e "s,^.*=${DIR}\$,," -e "/^$/d" -i "$FILE"
-      else
-          echo "Not found. ${FILE}"
-      fi
-  else
-      if [ -z "$DIR" ] || [ ! -d "$DIR" ]; then
-          echo "adddev dir [name]"
-          return 1
-      fi
-      if grep "^${NAME}=$DIR\$" "$FILE" >/dev/null 2>&1; then
-          echo "Unchanged. ($FILE)"
-      elif grep "^${NAME}=" "$FILE" >/dev/null 2>&1; then
-          echo "Changed. ($FILE)"
-          sed -e "s,^${NAME}=\.*$,$NAME=$DIR," -i "$FILE"
-      else
-          echo "Added. ($FILE)"
-          echo "${NAME}=${DIR}" >>"$FILE"
-      fi
-  fi
+    if [ "$ACTION" = "del" ]; then
+        if [ -z "$FILE" ]; then
+            echo "No shadows."
+        elif grep "^${DIR}=" "$FILE" >/dev/null 2>&1; then
+            echo "Removed. ${FILE}"
+            sed -e "s,^${DIR}=.*\$,," -e "/^$/d" -i "$FILE"
+        elif grep "^.*=${DIR}\$" "$FILE" >/dev/null 2>&1; then
+            echo "Removed. ${FILE}"
+            sed -e "s,^.*=${DIR}\$,," -e "/^$/d" -i "$FILE"
+        else
+            echo "Not found. ${FILE}"
+        fi
+    else
+        if [ -z "$DIR" ] || [ ! -d "$DIR" ]; then
+            echo "adddev dir [name]"
+            return 1
+        fi
+        if grep "^${NAME}=$DIR\$" "$FILE" >/dev/null 2>&1; then
+            echo "Unchanged. ($FILE)"
+        elif grep "^${NAME}=" "$FILE" >/dev/null 2>&1; then
+            echo "Changed. ($FILE)"
+            sed -e "s,^${NAME}=\.*$,$NAME=$DIR," -i "$FILE"
+        else
+            echo "Added. ($FILE)"
+            echo "${NAME}=${DIR}" >>"$FILE"
+        fi
+    fi
 }
 
 adddev() {
-  FILE="$HOME/.dev_directories"
-  add_lsdev -file "$FILE" "$@"
+    FILE="$HOME/.dev_directories"
+    add_lsdev -file "$FILE" "$@"
 }
 
 addbuild() {
-  FILE=`findancestor .shadows`
-  [ -z "$FILE" ] && FILE="$PWD/.shadows"
-  add_lsdev -file "$FILE" "$@"
+    FILE=`findancestor .shadows`
+    [ -z "$FILE" ] && FILE="$PWD/.shadows"
+    add_lsdev -file "$FILE" "$@"
 }
 
 #lsdev
 lsdev() {
-   if [ "$1" = "-e" ]; then
-       shift
-       EXECUTE="$1"
-       shift
-       LSDEV_PATH=`lsdev.pl -tp "$@"`
-       if [ -n "$LSDEV_PATH" ]; then
-           if echo "$EXECUTE" | grep -q "{}"; then
-               EXECUTE=`echo $EXECUTE | sed "s,{},$LSDEV_PATH,g"`
-           else
-               E=`echo $EXECUTE | cut -d' ' -f1`
-               if [ -x "$LSDEV_PATH/$E" ]; then
-                   EXECUTE="$LSDEV_PATH/$EXECUTE"
-               else
-                   EXECUTE="$EXECUTE $LSDEV_PATH"
-               fi
-           fi
-           echo "Executing: $EXECUTE" >&2
-           eval $EXECUTE
-       fi
-   else
-       lsdev.pl "$@"
-   fi
+    if [ "$1" = "-e" ]; then
+        shift
+        local EXECUTE="$1"
+        shift
+        local LSDEV_PATH=`lsdev.pl -tp "$@"`
+        if [ -n "$LSDEV_PATH" ]; then
+            if echo "$EXECUTE" | grep -q "{}"; then
+                EXECUTE=`echo $EXECUTE | sed "s,{},$LSDEV_PATH,g"`
+            else
+                E=`echo $EXECUTE | cut -d' ' -f1`
+                if [ -x "$LSDEV_PATH/$E" ]; then
+                    EXECUTE="$LSDEV_PATH/$EXECUTE"
+                else
+                    EXECUTE="$EXECUTE $LSDEV_PATH"
+                fi
+            fi
+            echo "Executing: $EXECUTE" >&2
+            eval $EXECUTE
+        fi
+    else
+        lsdev.pl "$@"
+    fi
 }
 lsd() {
-   OPTS="$@"
-   [ -z "$OPTS" ] && OPTS="-"
-   DIR=`lsdev -l $OPTS`
-   echo "$DIR"
+    local OPTS="$@"
+    [ -z "$OPTS" ] && OPTS="-"
+    local DIR=`lsdev -l $OPTS`
+    echo "$DIR"
 }
 
 editdev() {
-    EDIT=edit
+    local EDIT=edit
     if [ "$1" = "-vi" ]; then
-       EDIT=vi
-       shift
+        EDIT=vi
+        shift
     fi
-    FILE="$1"
+    local FILE="$1"
     if [ -z "$FILE" ]; then
         echo "Must specify file to edit"
         return 1
     fi
     shift
     if [ ! -e "$FILE" ]; then
-       DIR="-"
-       if echo "$FILE" | grep '^%' >/dev/null 2>&1; then
-          DIR=`echo "$FILE" | sed "s,^\(%.*/\).*,\1,g"`
-          FILE=`echo "$FILE" | sed "s,^$DIR,,"`
-       fi
-       DIR=`lsdev $DIR`
-       FILE="$DIR/$FILE"
+        DIR="-"
+        if echo "$FILE" | grep '^%' >/dev/null 2>&1; then
+            DIR=`echo "$FILE" | sed "s,^\(%.*/\).*,\1,g"`
+            FILE=`echo "$FILE" | sed "s,^$DIR,,"`
+        fi
+        DIR=`lsdev $DIR`
+        FILE="$DIR/$FILE"
     fi
     if [ -e "$FILE" ]; then
         $EDIT $@ $FILE
@@ -138,40 +138,44 @@ lsdevsetenvs() {
 }
 
 whatdev() {
-    NAME=`lsdev -p -ts $@`
+    local NAME=`lsdev -p -ts $@`
     export WHATDEV_LAST_ARGS="x${@}x${PWD}"
     export WHATDEV_LAST_NAME="$NAME"
 
     if [ -n "$NAME" ]; then
-      echo "$NAME"
+        echo "$NAME"
     else
-      echo "$PWD: Not a tree" >&2
-      return 1;
+        echo "$PWD: Not a tree" >&2
+        return 1;
     fi
     return 0
 }
 alias wdd="whatdev -ta"
 
 setdev() {
-    DIR=`lsdev -w $@`
+    local DIR=`lsdev -w $@`
     if [ -n "$DIR" ]; then
-      echo "Set: $DIR"
-      return 0
+        echo "Set: $DIR"
+        return 0
     fi
     echo "Not set."
     return 1
 }
 cddev() {
-    if [ "$#" = 1 ] && echo "$1" | grep '^@' >/dev/null 2>&1; then
-        MATCH=`echo $1 | sed "s,^@,,"`
-        DIR=`emacsclient -e "(sam-what-directory \"$MATCH\")" | sed 's,"\(.*\)",\1,'`
-    else
-        DIR=`lsdev -w $@`
+    local DIR=`lsdev -w $@`
+    if [ -z "$DIR" ]; then
+        if [ "$#" = "1" ] && [ -d "$1" ]; then
+            DIR="$1"
+        elif [ "$#" = 1 ] && echo "$1" | grep '^@' >/dev/null 2>&1; then
+            local MATCH=`echo $1 | sed "s,^@,,"`
+            DIR=`emacsclient -e "(sam-what-directory \"$MATCH\")" | sed 's,"\(.*\)",\1,'`
+        elif which pvm >/dev/null 2>&1; then
+            DIR=$(pvm --silent --no-autoupdate show --no-global --no-current --installed $@ -- installpath)
+        fi
     fi
-    [ -z "$DIR" ] && [ "$#" = "1" ] && [ -d "$1" ] && DIR="$1"
     if [ -d "$DIR" ] && [ -n "$DIR" ]; then
-      echo "$DIR"
-      cd "$DIR" && return 0
+        echo "$DIR"
+        cd "$DIR" && return 0
     fi
     echo "Not found."
     return 1
@@ -185,11 +189,11 @@ alias cdb=cddb
 makedev() {
     DIR=`lsdev -w $@`
     if [ -n "$DIR" ]; then
-      echo "$DIR"
-      make -C "$DIR" && return 1
+        echo "$DIR"
+        make -C "$DIR" && return 1
     else
-      echo "Not found."
-      return 1
+        echo "Not found."
+        return 1
     fi
 }
 alias mdd=makedev
