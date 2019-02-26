bab22928a9a86a4a4a874aea64b27a6002560733
Anders Bakken <agbakken@gmail.com>
2019-02-26 09:11:31 -0800

    update

--- bin/ubermake.sh
+++ bin/ubermake.sh
@@ -135,7 +135,7 @@ build() {
                     # echo "max is $max num is $num"
                     LINE=`ninja -t commands | grep " -c\>" 2>/dev/null | grep "\.o\>" 2>/dev/null | head -n1`
                     for i in $LINE; do
-                        [ ! -e "$i" ] && continue
+                        [ ! -e "$i" ] && [ ! -e "`which $i`" ] && continue
                         echo "$i" | grep --quiet "\\(.*ccache\\|.*rtags-gcc-prefix.sh\\|.*cc_prefix.sh\\|make$\\)" && continue
                         # echo "$i" | grep --quiet "\\(icecream\\|icecc\\|plast\\)" && break
 
@@ -150,7 +150,7 @@ build() {
                             i="$RESOLVED"
                         fi
                         case "$i" in
-                            *icecc|*plastc|*fisk)
+                            *icecc|*plastc|*fisk*)
                             # echo "It's icecream $f $i"
                             ;;
                             *) ### no build farm, lets reduce jobs
