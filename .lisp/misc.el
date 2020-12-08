632e5ef83675069452f9f67fcbc9d7c943f0c7f3
Anders Bakken <agbakken@gmail.com>
2020-12-08 13:46:18 -0800

    add project

--- .lisp/misc.el
+++ .lisp/misc.el
@@ -1203,6 +1203,7 @@ to case differences."
       (mkdir dir t)
       (with-temp-buffer
         (insert "cmake_minimum_required(VERSION 2.8)\n"
+                "project(" name ")\n"
                 "include_directories(${CMAKE_CURRENT_LIST_DIR})\n"
                 "set(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11\")\n"
                 "set(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS}\")\n"
