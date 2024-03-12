91749cb491dc832de5760d7e10223b8dfbfb49a4
Anders Bakken <agbakken@gmail.com>
2024-03-12 14:55:33 -0700

    gooder

--- bin/setup-brew-sshd.sh
+++ bin/setup-brew-sshd.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]} )"
 brew install openssh
 cat $SCRIPT_DIR/org.openssh.sshd.plist | sudo tee /Library/LaunchDaemons/org.openssh.sshd.plist
 cat $SCRIPT_DIR/sshd_config | sudo tee /opt/homebrew/etc/ssh/sshd_config
-sudo launchctl unload /Library/LaunchDaemons/org.openssh.sshd.plist
+sudo launchctl unload /Library/LaunchDaemons/org.openssh.sshd.plist &>/dev/null
 sudo launchctl load -w /Library/LaunchDaemons/org.openssh.sshd.plist
-echo "sshd is running at $(grep -o ^Port [0-9]\+)"
+echo 'sshd is running at $(grep -o "^Port [0-9]\+")'
