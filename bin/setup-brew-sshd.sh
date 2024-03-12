a4eb9a541bd3abab58a825968d1b2f278e9e3837
Anders Bakken <agbakken@gmail.com>
2024-03-12 14:50:41 -0700

    Bring back opensshd from brew.

---
+++ bin/setup-brew-sshd.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]} )"
+
+brew install openssh
+cat $SCRIPT_DIR/org.openssh.sshd.plist | sudo tee /Library/LaunchDaemons/org.openssh.sshd.plist
+cat $SCRIPT_DIR/sshd_config | sudo tee /opt/homebrew/etc/ssh/sshd_config
+sudo launchctl unload /Library/LaunchDaemons/org.openssh.sshd.plist
+sudo launchctl load -w /Library/LaunchDaemons/org.openssh.sshd.plist
+echo "sshd is running at $(grep -o ^Port [0-9]\+)"
