d9e61847c93a3d08dc01fdd27174a618826245fe
Anders Bakken <agbakken@gmail.com>
2025-08-02 09:25:04 -0700

    Use node instead. Only store sha1 and mtime. Don't rely on tar or git to restore.

---
+++ bin/prettier.config.js
@@ -0,0 +1,8 @@
+/* (c) 2024 Netflix, Inc. Do not copy or use without prior written permission from Netflix, Inc. */
+
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    proseWrap: "never",
+    trailingComma: "none"
+};
