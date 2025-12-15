f663756535ebf74970d87340103aced87e0eb134
Sam Magnuson <smagnuso@gmail.com>
2025-12-15 14:43:03 -0800

    try moving to straight for our packages

--- .lisp/misc-packages.el
+++ .lisp/misc-packages.el
@@ -1,69 +1,58 @@
 ;;===================
-;; package stuff
+;; package stuff (straight.el)
 ;;===================
 
-(require 'package)
+;; Disable package.el
+ (setq package-enable-at-startup nil)
 
-(add-to-list 'package-archives '("gnusecure" . "https://elpa.gnu.org/packages/"))
-;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
+;; Enable straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (defun misc-package-install (&optional query)
+  "Install a package using straight.el.
+With prefix arg QUERY, prompt for package name."
   (interactive "P")
   (let ((package (unless query (let ((symbol (symbol-at-point)))
                                  (and symbol
                                       (symbolp symbol)
                                       (intern (symbol-name symbol)))))))
     (if package
-        (package-install package)
-      (call-interactively 'package-install))))
+        (straight-use-package package)
+      (call-interactively 'straight-use-package))))
 
-(defun misc-current-packages ()
-  (unless package-activated-list
-    (package-refresh-contents))
-  (let ((ret)
-        (pkgs package-activated-list))
-    (while pkgs
-      (add-to-list 'ret (car pkgs))
-      (setq pkgs (cdr pkgs)))
-    (sort ret (lambda (a b) (compare-strings (symbol-name a) nil nil
-                                             (symbol-name b) nil nil)))))
+(defvar misc-packages nil
+  "List of packages managed by misc-init-packages.")
 
-(defun misc-create-desired-packages ()
-  (interactive)
-  (insert "(defvar misc-desired-packages (list ")
-  (let ((pkg (misc-current-packages))
-        (first t))
-    (while pkg
-      (if first
-          (setq first nil)
-        (insert "\n                                   "))
-      (insert "'" (symbol-name (car pkg)))
-      (setq pkg (cdr pkg)))
-    (insert "))\n")))
-
-(defvar misc-packages nil)
 (defun misc-init-packages (packages)
-  (let (updated)
-    (while packages
-      (push (car packages) misc-packages)
-      (when (not (package-installed-p (car packages)))
-        (when (not updated)
-          (setq updated t)
-          (package-refresh-contents))
-        (message "Installing missing package %s" (symbol-name (car packages)))
-        (package-install (intern (symbol-name (car packages)))))
-      (setq packages (cdr packages)))))
+  "Install PACKAGES using straight.el."
+  (dolist (pkg packages)
+    (push pkg misc-packages)
+    (straight-use-package pkg)))
 
 (defun misc-list-unaccounted-packages (&optional packages)
+  "List installed packages not in PACKAGES."
   (interactive)
   (unless packages
     (setq packages misc-packages))
-  (package-show-package-list
-   (cl-remove-if-not (lambda (x) (and (not (memq x packages))
-                                      (not (package-built-in-p x))
-                                      (package-installed-p x)))
-                  (mapcar 'car package-archive-contents))))
+  (let ((installed (hash-table-keys straight--recipe-cache)))
+    (message "Unaccounted packages: %s"
+             (cl-remove-if (lambda (x) (memq x packages)) installed))))
+
+(straight-use-package 'use-package)
+(straight-use-package 'el-patch)
 
 (provide 'misc-packages)
