7729c0416f6125250223faa062d0cc6f6d809f0d
Anders Bakken <agbakken@gmail.com>
2025-12-26 19:49:54 -0800

    Only source if the files exist

--- .lldb/misc.py
+++ .lldb/misc.py
@@ -3,5 +3,11 @@
 import lldb
 import os
 
+def source_if_exists(debugger, command, result, internal_dict):
+  path = os.path.expanduser(command.strip())
+  if os.path.exists(path):
+    debugger.HandleCommand('command source %s' % path)
+
 def __lldb_init_module (debugger, dict):
+  debugger.HandleCommand('command script add -f misc.source_if_exists source_if_exists')
   debugger.HandleCommand('command alias tbt t a a bt')
