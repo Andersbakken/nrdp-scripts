db7b390c731fbdc9527c21db2b0a99d9890e2303
Sam Magnuson <smagnuso@gmail.com>
2026-05-21 16:59:35 -0700

    guess -w works here too

--- bin/tmux/tmux-hardcopy.sh
+++ bin/tmux/tmux-hardcopy.sh
@@ -9,5 +9,5 @@ rm -f "$1"
 tmux -q capture-pane -J -S -32768 \; save-buffer $1 \; display-message "Captured to: $1"
 
 if [ "$EDIT" = "yes" ]; then
-    emacsedit.sh -n "$1" >/dev/null 2>&1
+    emacsedit.sh -w -n "$1" >/dev/null 2>&1
 fi
