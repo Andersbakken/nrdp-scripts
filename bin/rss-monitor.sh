93824057c54524dee6fff0b2a5160d5344a82889
Anders Bakken <agbakken@gmail.com>
2026-09-04 16:10:59 -0700

    goodness

--- bin/rss-monitor.sh
+++ bin/rss-monitor.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 PIDS=
 TIMER=1
 PRINT_HEADER=
 declare -a APPS
-
 while [ -n "$1" ]; do
     case "$1" in
         --timer|-t)
@@ -32,20 +30,35 @@ while [ -n "$1" ]; do
     esac
     shift
 done
-
+function kbToMb()
+{
+    if [ -z "$1" ]; then
+        echo "-"
+        return
+    fi
+    echo "$1 / 1024" | bc -l | sed -e 's,\(\.[0-9][0-9]\).*,\1,'
+}
+function smapsField()
+{
+    grep "^$2:" "/proc/$1/smaps_rollup" 2>/dev/null | sed -e "s,^$2:[^0-9]*\([0-9]\+\).*,\1,"
+}
 function doPid()
 {
     KB=`grep ^VmRSS: /proc/$1/status | sed -e 's,^VmRSS:[^0-9]*\([0-9]\+\).*,\1,'` 2>/dev/null
     [ -n "$KB" ] || return 1
-    MB=`echo "$KB / 1024" | bc -l | sed -e 's,\(\.[0-9][0-9]\).*,\1,'`
+    MB=`kbToMb "$KB"`
+    PRIVATE_DIRTY_KB=`smapsField "$1" Private_Dirty`
+    SHARED_DIRTY_KB=`smapsField "$1" Shared_Dirty`
+    PRIVATE_DIRTY_MB=`kbToMb "$PRIVATE_DIRTY_KB"`
+    SHARED_DIRTY_MB=`kbToMb "$SHARED_DIRTY_KB"`
     name=`grep ^Name: /proc/$1/status | sed -e 's,^Name: *,,'` 2>/dev/null
     if [ -n "$2" ] && [ -n "$PRINT_HEADER" ]; then
         echo "----------- `date +%r` -----------"
+        printf "%-24s %12s %12s %12s\n" "PID NAME" "RSS" "PRIVDIRTY" "SHAREDDIRTY"
     fi
-    echo -e "$1 $name\t${MB}MB"
+    printf "%-24s %12s %12s %12s\n" "$1 $name" "${MB}MB" "${PRIVATE_DIRTY_MB}MB" "${SHARED_DIRTY_MB}MB"
     return 0
 }
-
 while sleep "$TIMER"; do
     # echo "pids $PIDS apps ${APPS[@]}"
     FIRST=1
@@ -61,4 +74,3 @@ while sleep "$TIMER"; do
             fi
         done
     done
-done
